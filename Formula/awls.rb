d731ac359952f44d62b764fd1b4c9f5bf7b17f03
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-15 21:33:27 +0000

    Brew formula update for awls version v0.2.3

--- Formula/awls.rb
+++ Formula/awls.rb
@@ -5,25 +5,25 @@
 class Awls < Formula
   desc "Lookup EC2 information in the terminal"
   homepage ""
-  version "0.2.2"
+  version "0.2.3"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/danpilch/awls/releases/download/v0.2.2/awls_0.2.2_darwin_amd64.tar.gz"
-    sha256 "9f3f75ae3c319f2e873b307d51fda30e312715d1120c5032245143415bc882c9"
+    url "https://github.com/danpilch/awls/releases/download/v0.2.3/awls_0.2.3_darwin_amd64.tar.gz"
+    sha256 "ffd79183f238ff72ce43eb810897db9a77db76df3db1e51956988bfbd51ff583"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/danpilch/awls/releases/download/v0.2.2/awls_0.2.2_darwin_arm64.tar.gz"
-    sha256 "e53882f0f7e930619c3a0465ff85d1c59d74966cf525c82398591fdc60a5469a"
+    url "https://github.com/danpilch/awls/releases/download/v0.2.3/awls_0.2.3_darwin_arm64.tar.gz"
+    sha256 "4de45dd2aedc2844826d08fc8c241cb129ac0c6730eadfc34a4c2dedf6f831d7"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/danpilch/awls/releases/download/v0.2.2/awls_0.2.2_linux_amd64.tar.gz"
-    sha256 "dc32cef4a59efa77a45c35458d01f136ac055c6228629a2151dee812bc5d42d1"
+    url "https://github.com/danpilch/awls/releases/download/v0.2.3/awls_0.2.3_linux_amd64.tar.gz"
+    sha256 "0a8ae0316266cdf4c94cf6eef7716c7fa877ca43b70acb969030093df4a8f507"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/danpilch/awls/releases/download/v0.2.2/awls_0.2.2_linux_arm64.tar.gz"
-    sha256 "2931b599e26236f25ae09bd81776fc27460a83b4c4f473fcbf51f82fe9e08df9"
+    url "https://github.com/danpilch/awls/releases/download/v0.2.3/awls_0.2.3_linux_arm64.tar.gz"
+    sha256 "f2694706f4cfe735c23a18935e2133139bf6c1e3d2880dee55db9d80539df07b"
   end
 
   def install
